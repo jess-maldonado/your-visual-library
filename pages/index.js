8d2a64ec618ea3312163927b1dace30503e04ec4
jessmal11 <jess.maldonado11@gmail.com>
2020-08-11 17:58:20 -0400

    Feature/add upload (#1)
    * converted csv upload to json
    
    * edited readme
    
    * got parsing working
    
    * formatting
    
    Co-authored-by: jessmal11 <jess.maldonado1@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,44 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { FilePicker } from "evergreen-ui";
+
+let files = {};
+
+const csv = require("csvtojson");
+
+const csvToJson = (csv) => {
+  var lines = csv.split("\n");
+  var result = [];
+  var headers = lines[0].split(",");
+
+  for (var i = 1; i < lines.length; i++) {
+    var obj = {};
+    // Replacing commas with a space after with a space so that I can split by comma
+    // Replacing all quotes because they're unnecessary and not consistent in the data
+    let currentLine = lines[i].replace(/\, /g, " ").replace(/\"/g, "");
+    let splitLine = currentLine.split(",");
+
+    for (var j = 0; j < headers.length; j++) {
+      obj[headers[j]] = splitLine[j];
+    }
+    result.push(obj);
+  }
+  //return result; //JavaScript object
+  console.log(JSON.stringify(result)); //JSON
+};
+
+const readFiles = (files) => {
+  var reader = new FileReader();
+  reader.onload = function () {
+    // Display CSV file contents
+    csvToJson(reader.result);
+  };
+  reader.onerror = () => {
+    console.log(reader.error);
+  };
+
+  reader.readAsText(files[0]);
+};
 
 export default function Home() {
   return (
@@ -15,9 +54,19 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
+
+        <FilePicker
+          name="goodreads_library"
+          capture
+          accept="text/csv"
+          width={250}
+          marginBottom={32}
+          onChange={(files) => readFiles(files)}
+          placeholder="Select the file here!"
+        />
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -56,10 +105,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
